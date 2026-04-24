b2d4bf4b0add2766d6d5cce0f09ec3d07ddfd9dd
serhiikovalenko <kovalenkoserg1990@gmail.com>
2026-04-25 00:13:56 +0300

    working simple login register forms

--- frontend/src/client-page/components/LoginForm.vue
+++ frontend/src/client-page/components/LoginForm.vue
@@ -4,16 +4,53 @@ import Button from 'primevue/button';
 import InputGroup from 'primevue/inputgroup';
 import Password from 'primevue/password';
 import InputText from 'primevue/inputtext';
+import { useMutation } from '@tanstack/vue-query';
 import { reactive } from 'vue';
+import { useToast } from 'primevue/usetoast';
+import type { User } from '@/types';
+import api from '@/api';
 
-const formData = reactive({
-  email: '',
-  password: '',
+const toast = useToast();
+const initialState = { email: '', password: '' };
+const formData = reactive({ ...initialState });
+
+const mutation = useMutation({
+  mutationFn: async (user: Pick<User, 'email' | 'password'>) => {
+    const response = await api.post<User>('/login', user);
+    console.log(response, 'response useMutation client');
+    return response;
+  },
+
+  onSuccess: () => {
+    Object.assign(formData, initialState);
+    toast.add({
+      severity: 'success',
+      summary: 'Успіх',
+      detail: 'Користувача успішно залогігений',
+      life: 3000,
+    });
+  },
+  onError: (err: any) => {
+    const errorMessage = err.response?.data?.message || 'Помилка при вході';
+    console.log(err.error, ' err onError client');
+    console.log(errorMessage, ' errorMessage');
+    toast.add({
+      severity: 'error',
+      summary: 'Помилка',
+      detail: errorMessage,
+      life: 5000,
+    });
+    console.error('Login error:', err);
+  },
 });
+
+const handleSubmit = (): void => {
+  mutation.mutate({ ...formData });
+};
 </script>
 
 <template>
-  <form class="max-w-100 m-auto flex flex-col gap-2">
+  <form @submit.prevent="handleSubmit" class="max-w-100 m-auto flex flex-col gap-2">
     <div>Login Form</div>
     <InputGroup>
       <InputGroupAddon>
@@ -33,7 +70,12 @@ const formData = reactive({
         toggleMask
       />
     </InputGroup>
-    <Button label="Login" type="submit" severity="help" />
+    <Button
+      :disabled="mutation.isPending.value"
+      :label="mutation.isPending.value ? 'Вхід...' : 'Вхід'"
+      type="submit"
+      severity="help"
+    />
   </form>
 </template>
 
