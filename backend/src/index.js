5d7651ea8b3e3fb157df942363455465dbe52c6a
serhiikovalenko <kovalenkoserg1990@gmail.com>
2026-05-06 11:45:45 +0300

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   }),
 );
